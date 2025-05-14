714dc0bb6ad8ef005e3f8a40d66bd82dad6b6577
Nguyễn Hoàng Huy <114816906+nhhpbc7a1@users.noreply.github.com>
2025-05-15 03:22:11 +0700

    fix labtech side on deploy

--- routes/labtech/dashboard.route.js
+++ routes/labtech/dashboard.route.js
@@ -29,7 +29,7 @@ router.get('/', async (req, res) => {
             labTechnician.specialtyId
         );
         
-        res.render('vwLabTech/dashboard', {
+        res.render('vwLabtech/dashboard', {
             pendingCount: stats.pendingCount,
             inProgressCount: stats.inProgressCount,
             completedCount: stats.completedCount,
